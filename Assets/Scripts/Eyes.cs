76942e364ab5e8592e36154ba6ef688af1818d95
Brian Lai <brianlai30@outlook.com>
2018-03-21 17:45:25 -0700

    Added hotkey for zooming

--- Assets/Scripts/Eyes.cs
+++ Assets/Scripts/Eyes.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Eyes : MonoBehaviour {
 
+    private Camera eyes;
+    private float defaultFOV;
+
 	void Start() {
-		
+		eyes = GetComponent<Camera>();
+        defaultFOV = eyes.fieldOfView;
 	}
 	
 	void Update() {
-		
+		if (Input.GetButton("Zoom")) {
+            eyes.fieldOfView = defaultFOV / 1.5f;
+        } else {
+            eyes.fieldOfView = defaultFOV;
+        }
 	}
 
 }

0af230567dbbb9c182a122484a1cc0e1dd43cf37
Brian Lai <brianlai30@outlook.com>
2018-03-21 23:29:36 -0700

    Added audio files to public members

--- Assets/Scripts/RadioSystem.cs
+++ Assets/Scripts/RadioSystem.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class RadioSystem : MonoBehaviour {
 
+    public AudioClip initialHeliCall;
+    public AudioClip initialCallReply;
+
+    private AudioSource audioSource;
+
 	void Start() {
-		
+		audioSource = GetComponent<AudioSource>();
 	}
-	
-	void Update() {
-		
-	}
+
+    void OnMakeInitialHeliCall() {
+        print(name + " OnMakeInitialHeliCall");
+        audioSource.clip = initialHeliCall;
+        audioSource.Play();
+        Invoke("InitialReply", initialHeliCall.length + 1f);
+    }
+
+    void InitialReply() {
+        audioSource.clip = initialCallReply;
+        audioSource.Play();
+        BroadcastMessage("OnDispatchHelicopter");
+    }
 
 }

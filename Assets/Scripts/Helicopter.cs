0ed4b0b3b63fe5e9fc6b2567ca9e752bcd98d502
Brian Lai <brianlai30@outlook.com>
2018-03-21 19:23:13 -0700

    Added helicopter sounds

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -7,15 +7,18 @@ public class Helicopter : MonoBehaviour {
     public AudioClip callSound;
     
     private bool called = false;
+    private AudioSource audioSource;
 
 	void Start() {
-		
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	void Update() {
 		if (Input.GetButtonDown("CallHeli") && !called) {
             called = true;
-            print("Helicopter called");
+            Debug.Log("Helicopter called");
+            audioSource.clip = callSound;
+            audioSource.Play();
         }
 	}
 

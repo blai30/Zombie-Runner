0215cba0c3989792aa01f98bdf386f47f0870228
Brian Lai <brianlai30@outlook.com>
2018-03-21 17:41:28 -0700

    Added spawn points code and added Eyes.cs

--- Assets/Scripts/Eyes.cs
+++ Assets/Scripts/Eyes.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Eyes : MonoBehaviour {
 
 	void Start() {
 		
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Transform playerSpawnPoints; // Parent of the spawn points
+    public bool respawn = false;
+
+    private Transform[] spawnPoints;
+    private bool lastToggle = false;
+
+	void Start() {
+		spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform>();
+	}
+	
+	void Update() {
+        if (lastToggle != respawn) {
+            Respawn();
+            respawn = false;
+        } else {
+            lastToggle = respawn;
+        }
+	}
+
+    private void Respawn() {
+        int i = Random.Range(1, spawnPoints.Length);
+        transform.position = spawnPoints[i].transform.position;
+    }
+
+}

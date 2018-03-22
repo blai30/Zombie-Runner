c384f9d5e67fcc6b1046208e585e02761b8652e2
Brian Lai <brianlai30@outlook.com>
2018-03-21 20:36:24 -0700

    Added code to DayCycle.cs

--- Assets/Scripts/DayCycle.cs
+++ Assets/Scripts/DayCycle.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class DayCycle : MonoBehaviour {
 
+    [Tooltip("Number of minutes per second that pass, try 60")]
+    public float minutesPerSecond;
+
+    private Quaternion startRotation;
+
 	void Start() {
-		
+		startRotation = transform.rotation;
 	}
 	
 	void Update() {
-		
+        float angleThisFrame = Time.deltaTime / 360 * minutesPerSecond;
+		transform.RotateAround(transform.position, Vector3.forward, angleThisFrame);
 	}
 
 }

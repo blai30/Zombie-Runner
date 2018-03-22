2f5521f19aa95902e415254689f50be122fdce20
Brian Lai <brianlai30@outlook.com>
2018-03-21 17:51:57 -0700

    Imported udemy start pack

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+    public AudioClip callSound;
+    
+    private bool called = false;
+
+	void Start() {
+		
+	}
+	
+	void Update() {
+		if (Input.GetButtonDown("CallHeli") && !called) {
+            called = true;
+            print("Helicopter called");
+        }
+	}
+
+}
